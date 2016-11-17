7c500933c629cd461d4268f867d5a1be9d0f2846
alecbw <abarrett.wilsdon@gmail.com>
2016-11-17 14:15:15 -0800

    Add files via upload
    This calculates the area of a triangle or circle

--- Area Calculator.py
+++ Area Calculator.py
@@ -8,31 +8,32 @@ import time
 from datetime import datetime
 #this imports the date and time to print at the beginning of the program 
 now = datetime.now()
-print "It's on bitches"
+print "It's on"
 print '%s/%s/%s %s:%s' % (now.month, now.day, now.year, now.hour, now.minute)
 time.sleep(1)
 hint = "Don't forget to include the correct units! \nExiting..."
 #the \n prints it on a seperate line 
-option = raw_input("What shape do you want?").upper()
+option = raw_input("What shape do you want to analyze? Pick C for Circle and T for Triangle.").upper()
 if option == "C":
-	radius = float(raw_input ("what size radius"))
+	radius = float(raw_input ("What Length Radius?"))
   	area = pi*(radius**2)
     	print "the pie is baking..."
       	time.sleep(1)
         print str("%.2f" % area)
         print hint
 elif option == "T":
-	base = float(raw_input ("what size base"))
-	height = float(raw_input ("what size height"))
+	base = float(raw_input ("What Length Base?"))
+	height = float(raw_input ("What Length Height?"))
   	area = 0.5*base*height
     	print "Uni Bi Tri..."
       	time.sleep(1)
         print str("%.2f" % area)
         print hint
 else:
-	print "you're an asshole"
+	print "beep boop boop error code: Letters-R-Hard"
   	
 """oh man so much going on here
 so basically, the time.sleep causes the program to 'think' for one second
 the option = ensures if they enter C or T it is recorded, always in uppwer case
-then the giant if/else calculates the area of the circle by asking for a radius length and printing that to two decimal points (%.2f (floating point) as well as the hint)"""
+then the giant if/else calculates the area of the circle by asking for a radius length
+and printing that to two decimal points (%.2f (floating point) as well as the hint)"""
